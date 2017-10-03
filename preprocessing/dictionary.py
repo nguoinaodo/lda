dc9e08049384b2071793181bca06692edbd7c570
nguoinaodo <luugu196@gmail.com>
2017-10-03 15:56:37 +0700

    Test sklearn data

--- preprocessing/dictionary.py
+++ preprocessing/dictionary.py
@@ -7,7 +7,9 @@ V = len(lines)
 # Dictionary
 dictionary = {}
 inverse_dictionary = {}
+terms = []
 for i in range(V):
 	t = lines[i].strip()
+	terms.append(t)
 	dictionary[t] = i
 	inverse_dictionary[i] = t
---
+++ test_2.py
@@ -0,0 +1,28 @@
+import numpy as np
+from time import time
+
+# Data
+from preprocessing.readdata import documents_1 as W
+from preprocessing.dictionary import dictionary as dic, \
+		inverse_dictionary as inv_dic, terms
+
+def print_top_words(model, feature_names, n_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        message = "Topic #%d: " % topic_idx
+        message += " ".join([feature_names[i]
+                             for i in topic.argsort()[:-n_top_words - 1:-1]])
+        print(message)
+    print()
+
+# Model
+from sklearn.decomposition import LatentDirichletAllocation
+lda = LatentDirichletAllocation(n_components=100, max_iter=20,
+                                learning_method='online',
+                                learning_offset=50.,
+                                random_state=0)
+t0 = time()
+lda.fit(W)
+print("done in %0.3fs." % (time() - t0))
+
+print("\nTopics in LDA model:")
+print_top_words(lda, terms, 20)
